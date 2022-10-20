b531aab1528ac256f24171c090c8dba6393f51e1
cbarrios <lalobarrios16@yahoo.com>
2022-10-19 22:33:49 -0400

    master: (5) default list provided

---
+++ app/src/main/java/com/lalosapps/recyclerview/CardProvider.kt
@@ -0,0 +1,57 @@
+package com.lalosapps.recyclerview
+
+object CardProvider {
+
+    val cards = mutableListOf(
+        CardItem(
+            "Title 1",
+            "Description 1",
+            false
+        ),
+        CardItem(
+            "Title 2",
+            "Description 2",
+            false
+        ),
+        CardItem(
+            "Title 3",
+            "Description 3",
+            false
+        ),
+        CardItem(
+            "Title 4",
+            "Description 4",
+            false
+        ),
+        CardItem(
+            "Title 5",
+            "Description 5",
+            false
+        ),
+        CardItem(
+            "Title 6",
+            "Description 6",
+            false
+        ),
+        CardItem(
+            "Title 7",
+            "Description 7",
+            false
+        ),
+        CardItem(
+            "Title 8",
+            "Description 8",
+            false
+        ),
+        CardItem(
+            "Title 9",
+            "Description 9",
+            false
+        ),
+        CardItem(
+            "Title 10",
+            "Description 10",
+            false
+        )
+    )
+}
